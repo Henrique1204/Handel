26cce8fb31302643ad8ece8f9a5e63692e1e8a2e
Paulo <pauloh16.pdr@gmail.com>
2020-09-30 19:30:50 -0300

    Adicionar tema do wordpress

--- functions.php
+++ functions.php
@@ -1,2 +1,36 @@
-<?php 
+<?php
 
+// Adiciona o tema do woocommerce
+function handel_add_woocommerce_support() {
+    add_theme_support('woocommerce');
+}
+
+add_action('after_setup_theme', 'handel_add_woocommerce_support');
+
+// Define o css do site
+function handel_css() {
+    wp_register_style('handel-style', get_template_directory_uri().'/style.css', [], '1.0.0');
+
+    wp_enqueue_style('handel-style');
+}
+
+add_action('wp_enqueue_scripts', 'handel_css');
+
+// Manipula tamanho das imagens inseridas
+function handel_custom_image() {
+    // Nome, largura, altura, crop
+    // O array pode ser substituído por ture.
+    // O array recebe configurações, no caso diz pro crop ser apartir do centro e não cortar o topo
+    add_image_size('slide', 1000, 800, ['center', 'top']);
+    update_option('medium_crop', 1);
+}
+
+add_action('after_setup_theme', 'handel_custom_image');
+
+
+// Filtro pra limitar número de produtos mostrados em loja
+function handel_loop_shop_per_page() {
+    return 6;
+}
+
+add_filter('loop_shop_per_page', 'handel_loop_shop_per_page');
--- page-home.php
+++ page-home.php
@@ -4,7 +4,8 @@
 ?>
 
 <?php if(have_posts()) { while(have_posts()) { the_post(); ?>
-
+    <h1><?php the_title(); ?></h1>
+    <main><?php the_content(); ?></main>
 <?php } } ?>
 
 <?php get_footer(); ?>
